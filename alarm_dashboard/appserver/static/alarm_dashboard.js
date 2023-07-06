32adcac397a1dab9815ed08a16eae460bb7fefe6
barkai36 <barkai36@gmail.com>
2023-07-06 21:23:40 +0300

    Update alarm_dashboard.js
    Event listener functions runs many time.
    Added "off()" to remove the listener after every run

--- alarm_dashboard/appserver/static/alarm_dashboard.js
+++ alarm_dashboard/appserver/static/alarm_dashboard.js
@@ -11,6 +11,9 @@ require([
     "splunkjs/mvc",
     "splunkjs/mvc/simplexml/ready!"
 ], function(mvc) {
+	
+	var search_done_c = 1; //counter
+	var results_c = 1; //counter
 
 	var tokens = mvc.Components.get("default");
 	// Retrieve the value of a token $mytoken$
@@ -19,33 +22,33 @@ require([
 	alarm_search = mvc.Components.get("alarm_search")
 	var alarm_rows;
 	alarm_search.on('search:done', function(properties) {
-		// console.log("Search is done");
-		// Print the search job properties
-		//console.log("Alert search DONE!\nSearch job properties:", properties.content);
+		run_alarm_search(alarm_search);
 	})
-
-		
-	var alarm_results = alarm_search.data("results", {count:0});
-	first_run = 0; // Verify we run the sound function once. Sometimes on.data() occurs twice on the same search.
-	alarm_results.on( "data", function() {
-		if(first_run==0) {
-			first_run = 1;
-			alarm_rows = alarm_results.data().rows;
-			//console.log(alarm_rows);
-			incidents=alarm_rows[0][1]
-			state=alarm_rows[0][2]
-			play_sound_token = tokens.get("play_sound");
-			console.log("incidents:"+incidents+" , state:"+state+" , play_sound_token="+play_sound_token);
-			if(alarm_rows[0][2]=='Alarm' && play_sound_token=='play'){
+	
+	function run_alarm_search(alarm_search) {
+		//console.log("Search is done");
+		//console.log("search done # - "+search_done_c);
+		search_done_c+=1;
+		var alarm_results = alarm_search.data("results", {count:0});
+		alarm_results.on("data", function() {
+			run_alarm_on_results(alarm_results);
+		})
+	}
+	
+	function run_alarm_on_results(alarm_results) {
+		//console.log("Results arrived");
+		//console.log("results # - "+results_c);
+		results_c+=1;
+		//console.log(alarm_results._data.rows[0][2])
+		state=alarm_results._data.rows[0][2];
+		play_sound_token = tokens.get("play_sound");
+		if(state=='Alarm' && play_sound_token=='play'){
 				// Alarm state found, play the alarm sound
-				console.log("Alarm is enabled and found state:"+alarm_rows[0][2]+", playing sound.");
+				console.log("Alarm is enabled and found state:"+state+", playing sound.");
 				play_sound();
-			}
-			else {
-				console.log("Alarm in state:"+play_sound_token+" , Found state:"+alarm_rows[0][2]+", NOT playing sound.");
-			}
-			first_run=0;
 		}
-	});
+		alarm_results.off();
+	}
+	
 
 });
